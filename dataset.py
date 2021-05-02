709069b6a4efc6e733565c1f5446fe7cb33568c4
jpl <jiw010@ucsd.edu>
2021-05-02 16:44:45 +0800

    initial commit

---
+++ dataset.py
@@ -0,0 +1,12 @@
+import torch.utils.data as Data
+
+
+class Dataset(Data.Dataset):
+	def __init__(self):
+		super(Dataset, self).__init__()
+
+	def __len__(self):
+		return
+
+	def __getitem__(self, item):
+		return
